75e9ce7646f71bde37d751c27ac71351b0911b42
João Ferreira <joao@notifica.re>
2025-04-07 11:38:15 +0100

    feat: initial project configuration

---
+++ RELEASE.md
@@ -0,0 +1,9 @@
+# Release process
+
+1. Update the `maven-artifactVersion` in `gradle/libs.versions.toml`.
+2. Update the `ACTITO_VERSION` in `notificare/src/main/java/re/notifica/internal/Version.kt`.
+3. Update the `CHANGELOG.md`.
+4. Push the changes to the repo.
+5. Run `./gradlew clean`.
+6. Run `./gradlew publishReleasePublicationToS3Repository`.
+7. Create a GitHub release with the contents of the `CHANGELOG.md`.
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://maven.notifica.re/releases")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "actito-sdk-android"
+
+//include(":actito")
+//include(":actito-assets")
+//include(":actito-geo")
+//include(":actito-geo-beacons")
+//include(":actito-in-app-messaging")
+//include(":actito-inbox")
+//include(":actito-loyalty")
+//include(":actito-push")
+//include(":actito-push-ui")
+//include(":actito-scannables")
+//include(":actito-user-inbox")
+//include(":actito-utilities")
+//include(":sample")
+//include(":sample-user-inbox")
+
