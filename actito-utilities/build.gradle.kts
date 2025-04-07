718491d91a21d7d36b309d91ed1783ea857f4ab0
João Ferreira <joao@notifica.re>
2025-04-07 16:12:13 +0100

    feat(utilities): add library and tests

---
+++ actito-utilities/build.gradle.kts
@@ -0,0 +1,58 @@
+android {
+    namespace = "com.actito.utilities"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    buildToolsVersion = libs.versions.android.buildTools.get()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+        freeCompilerArgs += listOf(
+            "-Xexplicit-api=strict",
+        )
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines)
+
+    // Android
+    implementation(libs.androidx.core)
+
+    // Moshi
+    implementation(libs.bundles.moshi)
+    ksp(libs.moshi.codegen)
+
+    // Glide
+    implementation(libs.glide)
+    ksp(libs.glide.ksp)
+
+    // Tests
+    testImplementation(libs.junit)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.mockito)
+    testImplementation(libs.androidx.test.core)
+}
---
+++ actito-utilities/src/main/java/com/actito/utilities/networking/UserAgent.kt
@@ -0,0 +1,10 @@
+package com.actito.utilities.networking
+
+import android.content.Context
+import android.os.Build
+import com.actito.utilities.content.applicationName
+import com.actito.utilities.content.applicationVersion
+
+public fun Context.userAgent(sdkVersion: String): String {
+    return "$applicationName/$applicationVersion Actito/$sdkVersion Android/${Build.VERSION.RELEASE}"
+}
