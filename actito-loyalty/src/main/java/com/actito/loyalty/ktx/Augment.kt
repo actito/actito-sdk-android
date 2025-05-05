946efce5516f8a70d5167cb84b7d612a642d9521
João Ferreira <joao.ferreira@actito.com>
2025-05-05 09:39:25 +0100

    feat(loyalty): add library and tests

---
+++ actito-loyalty/src/main/java/com/actito/loyalty/ktx/Augment.kt
@@ -0,0 +1,24 @@
+package com.actito.loyalty.ktx
+
+import com.actito.Actito
+import com.actito.loyalty.ActitoLoyalty
+import com.actito.loyalty.internal.ActitoLoyaltyImpl
+
+@Suppress("unused")
+public fun Actito.loyalty(): ActitoLoyalty {
+    return ActitoLoyaltyImpl
+}
+
+// region Intent actions
+
+public val Actito.INTENT_ACTION_PASSBOOK_OPENED: String
+    get() = "re.notifica.intent.action.PassbookOpened"
+
+// endregion
+
+// region Intent extras
+
+public val Actito.INTENT_EXTRA_PASSBOOK: String
+    get() = "re.notifica.intent.extra.Passbook"
+
+// endregion
