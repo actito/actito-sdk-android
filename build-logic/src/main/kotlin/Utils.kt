4ef63b843cef7b2f9bc8150186217c6561191939
João Ferreira <joao.ferreira@actito.com>
2025-05-13 16:53:58 +0100

    chore: add access modifiers to utils

--- build-logic/src/main/kotlin/Utils.kt
+++ build-logic/src/main/kotlin/Utils.kt
@@ -1,9 +1,9 @@
 import java.io.File
 import java.util.Properties
 
-fun String.isStableVersion(): Boolean = "^([0-9]+)\\.([0-9]+)\\.([0-9]+)\$".toRegex().matches(this)
+internal fun String.isStableVersion(): Boolean = "^([0-9]+)\\.([0-9]+)\\.([0-9]+)\$".toRegex().matches(this)
 
-fun loadProperties(file: File) = Properties().apply {
+internal fun loadProperties(file: File) = Properties().apply {
     file.inputStream().use {
         load(it)
     }
