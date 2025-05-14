2de624ac37e87553d115d0e233b6f5591bdcfdff
João Ferreira <joao.ferreira@actito.com>
2025-05-14 15:44:26 +0100

    build: rename plugins

--- actito-inbox/build.gradle.kts
+++ actito-inbox/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("libraries")
+    id("actito-library")
     id("publish")
     id("linting")
 }
--- actito/build.gradle.kts
+++ actito/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("libraries")
+    id("actito-library")
     id("linting")
     id("publish")
 }
