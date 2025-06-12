6653cdb30f73da7c6a42b3bac217bcf15500b30c
João Ferreira <joao.ferreira@actito.com>
2025-06-12 08:40:55 +0100

    test(core): add test to ensure launch order

---
+++ actito/src/test/java/com/actito/internal/ActitoLaunchComponentTest.kt
@@ -0,0 +1,29 @@
+package com.actito.internal
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+public class ActitoLaunchComponentTest {
+    @Test
+    public fun testModuleOrder() {
+        val expectedOrder = listOf(
+            ActitoLaunchComponent.Module.DEVICE,
+            ActitoLaunchComponent.Module.SESSION,
+            ActitoLaunchComponent.Module.EVENTS,
+            ActitoLaunchComponent.Module.CRASH_REPORTER,
+            ActitoLaunchComponent.Module.PUSH,
+            ActitoLaunchComponent.Module.PUSH_UI,
+            ActitoLaunchComponent.Module.INBOX,
+            ActitoLaunchComponent.Module.ASSETS,
+            ActitoLaunchComponent.Module.SCANNABLES,
+            ActitoLaunchComponent.Module.GEO,
+            ActitoLaunchComponent.Module.LOYALTY,
+            ActitoLaunchComponent.Module.IN_APP_MESSAGING,
+            ActitoLaunchComponent.Module.USER_INBOX,
+        )
+
+        val actualOrder = ActitoLaunchComponent.Module.entries
+
+        assertEquals(expectedOrder, actualOrder)
+    }
+}
