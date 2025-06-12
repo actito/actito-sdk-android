e406d89d8ebc8463203c83d1cfb1330dc82c2180
João Ferreira <joao.ferreira@actito.com>
2025-06-12 08:09:13 +0100

    refactor(push-ui): move from ActitoModule to ActitoLaunchComponent

--- actito-push-ui/src/main/java/com/actito/push/ui/ktx/Augment.kt
+++ actito-push-ui/src/main/java/com/actito/push/ui/ktx/Augment.kt
@@ -1,12 +1,9 @@
 package com.actito.push.ui.ktx
 
 import com.actito.Actito
-import com.actito.internal.ActitoModule
-import com.actito.internal.modules.integrations.ActitoLoyaltyIntegration
 import com.actito.push.ui.ActitoInternalPushUI
 import com.actito.push.ui.ActitoPushUI
 import com.actito.push.ui.internal.ActitoPushUIImpl
-import com.actito.push.ui.internal.logger
 
 @Suppress("unused")
 public fun Actito.pushUI(): ActitoPushUI = ActitoPushUIImpl
@@ -14,24 +11,3 @@ public fun Actito.pushUI(): ActitoPushUI = ActitoPushUIImpl
 internal fun Actito.pushUIInternal(): ActitoInternalPushUI = pushUI() as ActitoInternalPushUI
 
 internal fun Actito.pushUIImplementation(): ActitoPushUIImpl = pushUI() as ActitoPushUIImpl
-
-internal fun Actito.loyaltyIntegration(): ActitoLoyaltyIntegration? {
-    if (!ActitoModule.Module.LOYALTY.isAvailable) {
-        logger.debug("Loyalty module is not available.")
-        return null
-    }
-
-    val instance = ActitoModule.Module.LOYALTY.instance ?: run {
-        logger.debug("Unable to acquire Loyalty module instance.")
-        return null
-    }
-
-    val integration = instance as? ActitoLoyaltyIntegration ?: run {
-        logger.debug(
-            "Loyalty module instance does not comply with the ActitoLoyaltyIntegration interface.",
-        )
-        return null
-    }
-
-    return integration
-}
