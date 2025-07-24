e646743e89b7c418565007fa193423b914f17664
João Ferreira <joao.ferreira@actito.com>
2025-07-24 09:09:02 +0100

    refactor(scannables): move away from java-compat classes

--- actito-scannables/src/main/java/com/actito/scannables/internal/Logger.kt
+++ actito-scannables/src/main/java/com/actito/scannables/internal/Logger.kt
@@ -1,11 +1,12 @@
 package com.actito.scannables.internal
 
+import com.actito.scannables.ActitoScannables
 import com.actito.utilities.logging.ActitoLogger
 
 internal val logger = ActitoLogger(
     tag = "ActitoScannables",
 ).apply {
     labelClassIgnoreList = listOf(
-        ActitoScannablesImpl::class,
+        ActitoScannables::class,
     )
 }
