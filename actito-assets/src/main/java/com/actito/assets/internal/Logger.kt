06994434e209e79bc70ed675163b6737715c4b04
João Ferreira <joao.ferreira@actito.com>
2025-07-24 08:53:22 +0100

    refactor(assets): move away from java-compat class

--- actito-assets/src/main/java/com/actito/assets/internal/Logger.kt
+++ actito-assets/src/main/java/com/actito/assets/internal/Logger.kt
@@ -1,11 +1,12 @@
 package com.actito.assets.internal
 
+import com.actito.assets.ActitoAssets
 import com.actito.utilities.logging.ActitoLogger
 
 internal val logger = ActitoLogger(
     tag = "ActitoAssets",
 ).apply {
     labelClassIgnoreList = listOf(
-        ActitoAssetsImpl::class,
+        ActitoAssets::class,
     )
 }
