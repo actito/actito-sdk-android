3168247d691e89ea39001dc1cb228828568215da
João Ferreira <joao.ferreira@actito.com>
2025-09-02 14:55:20 +0100

    refactor(core): move top-level KTX to ActitoEventsModule amd change receiver to JSONObject

--- actito/src/main/java/com/actito/ActitoEventsModule.kt
+++ actito/src/main/java/com/actito/ActitoEventsModule.kt
@@ -263,26 +263,22 @@ public object ActitoEventsModule {
             ),
         )
     }
-}
 
-/**
- * Creates an [ActitoEventData] instance from the given [JSONObject].
- *
- * **INTERNAL USE ONLY**
- *
- * This method is to only be used at framework-level integrations.
- *
- * This method deserializes the JSON string into an [ActitoEventData] object. If the JSON
- * cannot be parsed into a valid instance, an [IllegalArgumentException] will be thrown.
- *
- *  **IN
- *
- * @receiver The [ActitoEventData] context for the extension.
- * @param json The [JSONObject] containing the serialized [ActitoEventData].
- * @return A non-null [ActitoEventData] parsed from the given [JSONObject].
- * @throws IllegalArgumentException if the JSON cannot be deserialized into [ActitoEventData].
- */
-@Suppress("unused")
-public fun ActitoEventData.create(json: JSONObject): ActitoEventData {
-    return requireNotNull(ActitoEventsModule.dataAdapter.fromJson(json.toString()))
+    /**
+     * Creates an [ActitoEventData] instance from this [JSONObject].
+     *
+     * **INTERNAL USE ONLY**
+     *
+     * This method is to only be used at framework-level integrations.
+     *
+     * This method deserializes the JSON string into an [ActitoEventData] object. If the JSON
+     * cannot be parsed into a valid instance, an [IllegalArgumentException] will be thrown.
+     *
+     * @receiver The [JSONObject] containing the serialized [ActitoEventData].
+     * @return A non-null [ActitoEventData] parsed from this [JSONObject].
+     * @throws IllegalArgumentException if the JSON cannot be deserialized into [ActitoEventData].
+     */
+    public fun JSONObject.toEventData(): ActitoEventData {
+        return requireNotNull(dataAdapter.fromJson(this.toString()))
+    }
 }
